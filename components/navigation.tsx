545d7f9a67daebc732e8f37ef2346db02f57602d
keshavgarg24 <gargg.keshavv@gmail.com>
2025-07-16 12:52:16 +0530

    fixed buikd errors

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -5,7 +5,6 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 import { Menu, ChevronDown, Users, Star, UserPlus, CalendarDays } from "lucide-react"
 import Image from "next/image"
 import { useRouter } from "next/navigation"
-import { usePathname } from "next/navigation"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/dropdown-menu"
 
 const navItems = [
@@ -31,7 +30,6 @@ export default function Navigation() {
 	const [isInHero, setIsInHero] = useState(true)
 
 	const router = useRouter()
-	const pathname = usePathname();
 
 	useEffect(() => {
 		const handleScroll = () => {
